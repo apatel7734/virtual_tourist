12063be0d983996523033b68ad61e5b3e94b124c
Ashish <ashish@a.patel7734@gmail.com>
2015-05-31 13:06:24 -0700

    adding animated anotation

--- Virtual Tourist/LocationMapViewController.swift
+++ Virtual Tourist/LocationMapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 class LocationMapViewController: UIViewController,MKMapViewDelegate {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
@@ -18,13 +18,43 @@ class LocationMapViewController: UIViewController,MKMapViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         mapView.showsUserLocation = true
         mapView.delegate = self
+        addLongPressGestureToMapView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func addLongPressGestureToMapView(){
+        var longPressGesture = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        longPressGesture.minimumPressDuration = 1.0
+        self.mapView.addGestureRecognizer(longPressGesture)
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var annotationView = MKPinAnnotationView()
+        annotationView.animatesDrop = true
+        annotationView.setSelected(true, animated: true)
+        return annotationView
+    }
+    
+    func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer){
+        if gestureRecognizer.state != UIGestureRecognizerState.Began{
+            println("HandleLongPressGesture.Not Began")
+            return
+        }
+        println("HandleLongPressGesture.Began")
+        var touchPoint: CGPoint = gestureRecognizer.locationInView(self.mapView)
+        var touchMapCoord: CLLocationCoordinate2D  = self.mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+        addAnnotation(touchMapCoord)
+    }
+    
+    func addAnnotation(touchCoord: CLLocationCoordinate2D){
+       var placeMark = MKPlacemark(coordinate: touchCoord, addressDictionary: nil)
+        self.mapView.addAnnotation(placeMark)
+    }
+    
+    
 }
 
