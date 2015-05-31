3b3f904872d9eae8a6248b649b963c58d5f895ca
Ashish <ashish@a.patel7734@gmail.com>
2015-05-31 12:24:23 -0700

    initial commit

---
+++ Virtual Tourist/LocationMapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LocationMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ashish Patel on 5/10/15.
+//  Copyright (c) 2015 Average Techie. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
