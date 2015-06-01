a1fd0d95139d0b9b66eb62eea99b46cd0d11cb27
Ashish <ashish@a.patel7734@gmail.com>
2015-06-01 14:57:06 -0700

    setup collection cell

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -9,24 +9,75 @@
 import UIKit
 import MapKit
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     
     var annotation: MKAnnotation?
+    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
     
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
         if let pin = annotation{
             println("Pin = \(pin.coordinate.latitude) , \(pin.coordinate.longitude)")
             mapView.addAnnotation(pin)
             mapView.centerCoordinate = pin.coordinate
         }
-        // Do any additional setup after loading the view.
+        
+        photoCollectionView.dataSource = self
+        photoCollectionView.delegate = self
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        println("viewDidLayoutSubviews.")
+        //layout subviews so its cells take up of 1/3 of width.
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let width = floor(self.photoCollectionView.frame.size.width/3)
+        layout.itemSize = CGSize(width: width, height: width)
+        self.photoCollectionView.collectionViewLayout = layout
+    }
+    
+    //MARK - collection view delegates
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        println("collectionView. indexPath = \(indexPath.row)")
+        var photoCell = collectionView.dequeueReusableCellWithReuseIdentifier("photocell", forIndexPath: indexPath) as! PhotoCell
+        
+        configureCell(photoCell)
+        
+        return photoCell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+    func configureCell(cell: PhotoCell){
+        cell.progressView.layer.cornerRadius = 5.0
+        cell.progressView.backgroundColor = UIColor.darkGrayColor()
+        
     }
     
 }
---
+++ Virtual Tourist/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  Virtual Tourist
+//
+//  Created by Ashish Patel on 6/1/15.
+//  Copyright (c) 2015 Average Techie. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var photoImgView: UIImageView!
+    
+    @IBOutlet weak var progressView: UIView!
+
+}
